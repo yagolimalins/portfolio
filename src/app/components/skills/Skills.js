328faf744050eebdc9d8c9e885ce28eb1d9c9677
Yago Lima Lins <yago.lima.lins@protonmail.com>
2024-12-31 11:09:46 -0300

    implements skills section

--- src/app/components/skills/Skills.js
+++ src/app/components/skills/Skills.js
@@ -1,9 +1,34 @@
+import Skill from '../skill/Skill'
 import styles from './Skills.module.scss'
 
 const Skills = () => {
+
     return (
         <div className={styles.skills}>
-
+            <div className={styles.content}>
+                <div className={styles.textual}>
+                    <h2 className={styles.title}>
+                        Tecnologias
+                    </h2>
+                    <p className={styles.description}>
+                        Linguagens, frameworks e ferramentas:
+                    </p>
+                </div>
+                <div className={styles.icons}>
+                    <Skill skill={'JavaScript'} icon={'javascript'} />
+                    <Skill skill={'TypeScript'} icon={'typescript'} />
+                    <Skill skill={'React.js'} icon={'react'} />
+                    <Skill skill={'Next.js'} icon={'next'} />
+                    <Skill skill={'Node.js'} icon={'node'} />
+                    <Skill skill={'Express.js'} icon={'express'} />
+                    <Skill skill={'PostgreSQL'} icon={'postgresql'} />
+                    <Skill skill={'MongoDB'} icon={'mongodb'} />
+                    <Skill skill={'Sass/Scss'} icon={'sass'} />
+                    <Skill skill={'Tailwind'} icon={'tailwind'} />
+                    <Skill skill={'Figma'} icon={'figma'} />
+                    <Skill skill={'Git'} icon={'git'} />
+                </div>
+            </div>
         </div>
     )
 }
