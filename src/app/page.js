81ae33738671e1efa2716f8dbdb2a4c52d7f590c
Yago Lima Lins <yago.lima.lins@protonmail.com>
2025-01-01 01:42:52 -0300

    import inter font

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
-import About from "./components/about/About"
+import { Inter } from 'next/font/google'
 import Header from "./components/header/Header"
 import Hero from "./components/hero/Hero"
 import Skills from "./components/skills/Skills"
 
+const inter = Inter({ subsets: ['latin'] })
+
 const Home = () => {
     return (
-        <>
+        <div className={inter.className}>
             <Header />
             <Hero />
             <Skills />
-        </>
+        </div>
     )
 }
 
