0f25c229f8a75408bf3a010fb470a1f1ab6df728
Yago Lima Lins <yago.lima.lins@protonmail.com>
2025-01-08 16:53:22 -0300

    adds speed insights to project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import 'styles/reset.scss';
 import 'styles/variables.scss';
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className='app'>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
