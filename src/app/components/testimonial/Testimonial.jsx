cd656f888f3a953af4cadff2c3ba1b62284a978e
Yago Lima Lins <yago.lima.lins@protonmail.com>
2025-01-10 17:57:50 -0300

    updates testimonials section

--- src/app/components/testimonial/Testimonial.jsx
+++ src/app/components/testimonial/Testimonial.jsx
@@ -1,22 +1,33 @@
 import Image from 'next/image'
 import styles from './Testimonial.module.scss'
+import linkedin from 'assets/icons/linkedin.svg'
 
-const Testimonial = ({ avatar, testimonial, author, position }) => {
+const Testimonial = ({ avatar, testimonial, author, website, position }) => {
     return (
         <div className={styles.card}>
             <div className={styles.content}>
-                <Image
-                className={styles.avatar}
-                    src={avatar}
-                    alt={"Foto de cliente"}
-                />
+                <a href={website}>
+                    <Image
+                        className={styles.avatar}
+                        src={avatar}
+                        alt={"Foto de cliente"}
+                    />
+                </a>
 
                 <div className={styles.text}>
                     <p className={styles.testimonial}>
                         {`"${testimonial}"`}
                     </p>
                     <p className={styles.author}>
-                        {author}
+                        <a href={website} className={styles.website}>
+                            <Image
+                            className={styles.linkedin}
+                            src={linkedin}
+                            alt='Icone do LinkedIn'
+                            />
+                            
+                            {author}
+                        </a>
                     </p>
                     <p className={styles.position}>
                         {position}
--- src/app/sections/testimonials/Testimonials.jsx
+++ src/app/sections/testimonials/Testimonials.jsx
@@ -1,8 +1,8 @@
 import Headings from 'components/headings/Headings'
 import styles from './Testimonials.module.scss'
 import Testimonial from 'components/testimonial/Testimonial'
-import carla from 'assets/avatars/carla.png'
-import yago from 'assets/avatars/yago.jpg'
+import edgar from 'assets/avatars/edgar.jpg'
+import leonardo from 'assets/avatars/leonardo.jpg'
 
 const Testimonials = () => {
     return (
@@ -15,16 +15,26 @@ const Testimonials = () => {
                     />
                     <div className={styles.testimonials}>
                         <Testimonial
-                            avatar={carla}
-                            author={"Carla Britto"}
-                            testimonial={"Gostoso e dedicado ao trabalho"}
-                            position={'Minha namorada'}
+                            avatar={edgar}
+                            author={"Edgar Azevedo"}
+                            website={"https://www.linkedin.com/in/edgar-azevedo-dos-santos-filho-140a2b128"}
+                            testimonial={
+                                `A ferramenta de georreferenciamento criada por Yago foi um divisor de águas para a equipe. 
+                                Com ela, é possível acessar localizações em um piscar de olhos, planejar visitas de forma estratégica e monitorar unidades de longe. 
+                                Isso não apenas melhorou a produtividade, mas também tornou o trabalho muito mais eficiente e prático.`
+                            }
+                            position={'Coordenador - CIGIP'}
                         />
                         <Testimonial
-                            avatar={yago}
-                            author={"Yago Lins"}
-                            testimonial={"É verdade esse bilhete"}
-                            position={'Web Developer'}
+                            avatar={leonardo}
+                            author={"Dr. Leonardo Medeiros"}
+                            website={"https://www.linkedin.com/in/leonardo-medeiros-dr-6088443"}
+                            testimonial={
+                                `Yago é um profissional objetivo com habilidades diversas e pode contribuir para seus projetos como Músico ou Engenheiro de Software. 
+                                Ele foi meu Professor de Piano, mas, em nossas conversas, identifiquei fortes habilidades em engenharia de software. 
+                                Portanto, ele tem a minha recomendação para trabalhar como Engenheiro de Software ou como Músico para Composição de Jogos.`
+                            }
+                            position={'Professor de TI - IFAL'}
                         />
                     </div>
                 </div>
