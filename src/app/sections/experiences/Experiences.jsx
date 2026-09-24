aceba14caba6f8100578f876027d42477ec325be
Yago Lima Lins <yago.lima.lins@protonmail.com>
2026-09-23 21:07:40 -0300

    feat: delete another card

--- src/app/sections/experiences/Experiences.jsx
+++ src/app/sections/experiences/Experiences.jsx
@@ -9,20 +9,7 @@ const Experiences = () => {
         <div className={styles.textual}>
           <Headings h2={'Experiences'} h3={'My most recent experiences'} />
 
-          <Experience
-            logo={'tugonn.png'}
-            site={'https://tugonn.com'}
-            job={'Full Stack Software Engineer'}
-            tasks={[
-              'Developed web applications using Next.js, React.js, and Tailwind CSS',
-              'Managed complex application state with Zustand and React Query',
-              'Built RESTful APIs with Node.js and Express, validated by Zod',
-              'Implemented real-time features using Socket.io and JWT authentication',
-              'Designed MongoDB databases with Mongoose and integrated AWS S3/Cloudinary',
-              'Collaborated via GitHub and documented APIs using Postman and Swagger',
-            ]}
-            date={'2026 - Present'}
-          />
+
         </div>
       </div>
     </footer>
